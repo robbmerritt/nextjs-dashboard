dcef00a42a4397bf811d53c87627f73c7ce501f9
Robb Merritt <transempiric@gmail.com>
2023-10-26 20:23:21 -0700

    CSS ups.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4">
+      <div className="flex shrink-0 items-end rounded-lg bg-blue-500 p-4">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <RocketLaunchIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[32px] px-10">Robb Merritt&#39;s Portfolio</p>
+      <p className="text-[32px] px-4">Robb Merritt&#39;s Portfolio</p>
     </div>
   );
 }
