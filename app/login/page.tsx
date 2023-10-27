329b8bdbe7b120692418299656c26c368dd4ad3b
Robb Merritt <transempiric@gmail.com>
2023-10-26 20:26:46 -0700

    CSS ups color.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex w-full items-end rounded-lg bg-blue-500 p-3">
+        <div className="flex w-full items-end rounded-lg bg-green-500 p-3">
           <div className="text-white">
             <AcmeLogo />
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex shrink-0 items-end rounded-lg bg-blue-500 p-4">
+      <div className="flex shrink-0 items-end rounded-lg bg-green-500 p-4">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -23,7 +23,7 @@ export default function Page() {
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-green-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span>
           </Link>
