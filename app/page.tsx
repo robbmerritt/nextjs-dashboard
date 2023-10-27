947ac70ce3254cac9fae655c020c2f496b759838
Robb Merritt <transempiric@gmail.com>
2023-10-26 20:01:13 -0700

    Clean up 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import Link from 'next/link';
-import styles from './ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
