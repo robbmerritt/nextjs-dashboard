c781fb177db63f981536d9ba0c8056b891dd39ee
Robb Merritt <transempiric@gmail.com>
2023-10-26 19:58:48 -0700

    Clean up

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,20 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className={styles.shape}></div>
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Robb&#39;s Portfolio.</strong>
+          </p>
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
+            This is an example of Next.js 14 and Vercel.
           </p>
           <Link
             href="/login"
@@ -32,16 +32,20 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
+            src="/image/robb.png"
+            width={322}
+            height={322}
             className="hidden md:block"
+            style={{
+              objectFit: 'cover',
+              borderRadius: '100px', //👈 and here you can select border radius
+            }}
             alt="Screenshots of the dashboard project showing desktop and mobile versions"
           />
           <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
+            src="/image/robb.png"
+            width={322}
+            height={322}
             className="block md:hidden"
             alt="Screenshot of the dashboard project showing mobile version"
           />
